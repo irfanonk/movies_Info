fa2f6376551db352a655686204fd87aa4e3388de
irfan <onk.irfan@gmail.com>
2020-03-24 00:14:15 +0300

    Updates

---
+++ precache-manifest.db3cfa78fcfa07ef1cc6040edbc1f66f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3990c609298d51ffca6bf3cce1ac5cde",
+    "url": "/movies_Info/index.html"
+  },
+  {
+    "revision": "6a7fee13f06342bb4e08",
+    "url": "/movies_Info/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a1425329a022e901f417",
+    "url": "/movies_Info/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6a7fee13f06342bb4e08",
+    "url": "/movies_Info/static/js/2.23c6f023.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/movies_Info/static/js/2.23c6f023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1425329a022e901f417",
+    "url": "/movies_Info/static/js/main.3510b549.chunk.js"
+  },
+  {
+    "revision": "a56d8ac030c88a20745c",
+    "url": "/movies_Info/static/js/runtime-main.9c2fdd07.js"
+  }
+]);
